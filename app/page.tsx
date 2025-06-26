56ad869d6e9f9a41304a2c93c80b3e861b3e7bdb
Raphael Breham <raphael.breham@viacesi.fr>
2025-06-26 17:16:19 +0200

    LETS GO

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js bro!</a>
+          LETS <a href="https://nextjs.org">GOOOOOOOOOOOOOO</a>
         </h1>
 
         <p className={styles.description}>
