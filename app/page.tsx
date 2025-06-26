910ed120b1c163f25868411628f92112704f1836
Raphael Breham <raphael.breham@viacesi.fr>
2025-06-26 23:44:53 +0200

    dthsrhdr

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js Dev!</a>
+          Welcome to <a href="https://nextjs.org">Next.js Dev2!</a>
         </h1>
 
         <p className={styles.description}>
